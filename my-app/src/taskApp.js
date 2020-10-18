ef84d043bf436b1b7f6f1fe440a1011048441ee8
Carlos Graybill <carlos.a.graybill@gmail.com>
2020-10-18 18:04:28 -0400

    the task app works!

--- my-app/src/taskApp.js
+++ my-app/src/taskApp.js
@@ -1,6 +1,6 @@
 import React from "react";
-import addTask from "./components/addTask";
-import taskList from "./components/TaskList";
+import AddTask from "./components/addTask";
+import TaskList from "./components/TaskList";
 import Filters from "./components/Filters";
 import "./App.css";
 
@@ -8,8 +8,8 @@ export default function TaksApp() {
     return (
         <div className="task-app">
             <h1>Your Tasks!</h1>
-            <addTask />
-            <taskList />
+            <AddTask />
+            <TaskList />
             <Filters />
         </div>
     );
