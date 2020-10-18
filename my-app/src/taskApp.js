fa1073f8a072e5dee52080736525aedff66405ae
Carlos Graybill <carlos.a.graybill@gmail.com>
2020-10-18 19:32:22 -0400

    nav bar deletions

--- my-app/src/taskApp.js
+++ my-app/src/taskApp.js
@@ -2,13 +2,11 @@ import React from "react";
 import AddTask from "./components/addTask";
 import TaskList from "./components/TaskList";
 import Filters from "./components/Filters";
-import Nav from "./components/Nav"
 import "./App.css";
 
 export default function TaksApp() {
     return (
         <div className="task-app">
-            <Nav/>
                  <h1>Your Tasks!</h1> <br/>
             <Filters /> <br></br>
             <AddTask />
